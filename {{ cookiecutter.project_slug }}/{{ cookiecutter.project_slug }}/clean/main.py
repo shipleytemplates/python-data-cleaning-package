709e3b77c1498bc1372953e44dd5fd2cd6d26070
Garrett Shipley <garrett.shipley7+github@gmail.com>
2022-08-18 15:21:55 -0400

    Added clean subpackage

---
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/clean/main.py
@@ -0,0 +1,8 @@
+"""Module for the core logic of the cleaning functions."""
+
+import typing as t
+from pathlib import Path
+
+
+def main(input_files: list[Path], output_directory: t.Optional[Path]):
+    pass
